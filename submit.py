46873fb4c44596105a07896e7d1fdaaae3f08e67
Matthew McPherrin <mimcpher@csclub.uwaterloo.ca>
2011-06-03 20:05:55 -0400

    Added a form to submit

--- submit.py
+++ submit.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python
+
+import cgi
+import os
+
+print "Content-type: text/html\n\n"
+
+form = cgi.FieldStorage()
+
+data = cgi.escape(form["data"].value)
+
+user = os.environ["REMOTE_USER"]
+
+print user, "is now working on", data
+
+d = shelve.open("projects")
+
+d['user'] = data
+
+d.close()
